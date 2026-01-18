3b3de7758cea0e65ca3ec29c8d63f93b02ed2be2
BibekAryal99 <163297259+BibekAryal99@users.noreply.github.com>
2026-01-18 21:58:35 +0545

    Add type annotation to SummaryCards props

--- components/SummaryCards.tsx
+++ components/SummaryCards.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import type { SummaryCard, Props } from "@/app/types/type";
 
-export const SummaryCards = ({ data }) => {
+export const SummaryCards = ({ data }: Props) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4">
       {data.map((item, index) => (
