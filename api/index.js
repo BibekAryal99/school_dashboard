54b77ca1958429d20fcbe5ad184075f071e03564
BibekAryal99 <163297259+BibekAryal99@users.noreply.github.com>
2026-01-31 01:57:00 +0545

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,5 @@
-import jsonServer from "json-server";
-import path from "path";
-import { fileURLToPath } from "url";
+import db from "../db.json";
 
 export default function handler(req, res) {
-  const server = jsonServer.create();
-
-  // Calculate __dirname in ESM
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
-  const router = jsonServer.router(path.join(__dirname, "..", "db.json"));
-  const middlewares = jsonServer.defaults();
-
-  server.use(middlewares);
-  server.use(router);
-
-  // Pass the request to json-server
-  server(req, res);
+  res.status(200).json(db);
 }
