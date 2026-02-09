8f6651944dba5ca7d9f7bb7143a572dfce2cb177
BibekAryal99 <163297259+BibekAryal99@users.noreply.github.com>
2026-02-10 02:11:56 +0545

    Update page.tsx

--- app/(dashboard)/students/[id]/edit/page.tsx
+++ app/(dashboard)/students/[id]/edit/page.tsx
@@ -192,7 +192,7 @@ export default function StudentEditPage() {
                     <option value="A">A</option>
                     <option value="B+">B+</option>
                     <option value="B">B</option>
-                    <option value="C">C+</option>
+                    <option value="C+">C+</option>
                     <option value="C">C</option>
                     <option value="D">D</option>
                     <option value="F">F</option>
